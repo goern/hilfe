58c9dc2f467b45baf099a932f800ad6ae17c2eed
Christoph Görn <goern@b4mad.net>
2013-08-26 19:36:17 +0200

    reorganized test stuff

---
+++ hilfeApplicationTests/hilfeApplicationTests.h
@@ -0,0 +1,13 @@
+//
+//  hilfeApplicationTests.h
+//  hilfeApplicationTests
+//
+//  Created by Christoph Görn on 26.08.13.
+//  Copyright (c) 2013 #B4mad.Net. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface hilfeApplicationTests : SenTestCase
+
+@end
---
+++ hilfeApplicationTests/hilfeApplicationTests.m
@@ -0,0 +1,32 @@
+//
+//  hilfeApplicationTests.m
+//  hilfeApplicationTests
+//
+//  Created by Christoph Görn on 26.08.13.
+//  Copyright (c) 2013 #B4mad.Net. All rights reserved.
+//
+
+#import "hilfeApplicationTests.h"
+
+@implementation hilfeApplicationTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in hilfeApplicationTests");
+}
+
+@end
